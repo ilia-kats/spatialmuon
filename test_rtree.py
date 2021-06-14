c55da96e17e34565a340b6204e67d28247b6d01f
Ilia Kats <ilia-kats@gmx.net>
2021-06-14 15:22:15 +0200

    implement HDF5-serializable in-memory RTree storage

--- test_rtree.py
+++ test_rtree.py
@@ -48,15 +48,17 @@ class HDF5Storage(index.CustomStorage):
         if page == self.NewPage:
             self._pageid += 1
             self._grp[str(self._pageid)] = data
-            return self._pageid
+            page = self._pageid
+            spage = str(page)
         else:
             spage = str(page)
             if spage not in self._grp:
                 returnError.value = self.InvalidPageError
                 return 0
             del self._grp[spage]
-            self._grp[spage] = data
-            return page
+        dset = self._grp.create_dataset(spage, (len(page),), dtype=np.uint8, chunks=(len(page),), compression="gzip", compression_opts=9)
+        dset[()] = data
+        return page
 
     def deleteByteArray(self, page, returnError):
         """ Deletes a page """
@@ -65,13 +67,93 @@ class HDF5Storage(index.CustomStorage):
         except KeyError:
             returnError.contents.value = self.InvalidPageError
 
-f = h5py.File("../test.h5", "a", libver="latest")
-storage = HDF5Storage(f, "index")
-p = index.Property(type=index.RT_RTree, variant=index.RT_Star, dimension=2, pagesize=4096)
+    def flush(self, returnError):
+        pass
+
+class SerializableStorage(index.CustomStorage):
+    def __init__(self, grp=None):
+        self._pages = []
+        self._emptypages = []
+
+        if grp is not None:
+            self.from_hdf5(grp)
+
+    @property
+    def hasData(self):
+        return len(self._pages) > 0
+
+    def create(self, returnError):
+        pass
+
+    def destroy(self, returnError):
+        pass
+
+    def clear(self):
+        self._pages = []
+        self._emptypages = []
+
+    def loadByteArray(self, page, returnError):
+        try:
+            return self._pages[page]
+        except IndexError:
+            returnError.contents.value = self.InvalidPageError
+
+    def storeByteArray(self, page, data, returnError):
+        if page == self.NewPage:
+            if len(self._emptypages) > 0:
+                page = self._emptypages[-1]
+                del self._emptypages[-1]
+            else:
+                self._pages.append(None)
+                page = len(self._pages) - 1
+        try:
+            self._pages[page] = data
+            return page
+        except IndexError:
+           returnError.contents.value = self.InvalidPageError
+           return 0
+
+    def deleteByteArray(self, page, returnError):
+        try:
+            self._pages[page] = None
+            self._emptypages.append(page)
+        except IndexError:
+            returnError.contents.value = self.InvalidPageError
+
+    def flush(self, returnError):
+        pass
+
+    def to_hdf5(self, parent, name):
+        if name in parent:
+            del parent[name]
+        grp = parent.create_group(name, track_order=True)
+        for i, page in enumerate(self._pages):
+            if page is not None:
+                dset = grp.create_dataset(str(i), (len(page),), dtype=np.uint8, chunks=(len(page),), compression="gzip", compression_opts=9)
+                dset[()] = np.frombuffer(page, dtype=np.uint8)
+        grp.attrs["encoding-type"] = "rtree-index"
+        grp.attrs["encoding-version"] = "0.1.0"
+
+    def from_hdf5(self, grp):
+        for page in grp.values():
+            self._pages.append(page[()].tobytes())
+
+
+storage = SerializableStorage()
+p = index.Property(type=index.RT_RTree, variant=index.RT_Star, dimension=2)
 
 idx = index.Index(storage, interleaved=True, properties=p)
 
-coords = np.random.random(size=(1000000, 2))
-for i, c in enumerate(coords):
+coords = np.random.random(size=(10000, 2))
+from tqdm import tqdm
+for i, c in enumerate(tqdm(coords)):
     idx.insert(i, np.hstack((c,c)))
 
+
+f = h5py.File("../test.h5", "a", libver="latest")
+# storage = HDF5Storage(f, "index")
+idx.customstorage.to_hdf5(f, "index")
+
+storage = SerializableStorage(f["index"])
+idx = index.Index(storage, interleaved=True, properties=p)
+print(list(idx.intersection((0.4, 0.4, 0.5, 0.5))))

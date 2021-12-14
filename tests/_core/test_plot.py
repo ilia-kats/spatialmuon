102581dfd44d27b80e9687081695dc4b1539921b
Tim Treis <tim.treis@outlook.de>
2021-12-14 15:21:15 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/_core/test_plot.py
+++ tests/_core/test_plot.py
@@ -2,10 +2,9 @@ import unittest
 
 
 class PlottingTestClass(unittest.TestCase):
-
     def test_fov_preview(self):
         self.assertEqual(1, 1)
 
-        
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()

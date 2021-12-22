5926541ff55972d587f29af6ec2db73188a5e0ed
Tim Treis <tim.treis@outlook.de>
2021-12-22 23:37:44 +0100

    Update masks.py

--- spatialmuon/_core/masks.py
+++ spatialmuon/_core/masks.py
@@ -30,7 +30,7 @@ class Masks(BackableObject):
             else:
                 raise UnknownEncodingException(masktype)
         else:
-            return super().__new__(cls)
+            return super().__init__(cls)
 
     def __init__(
         self,

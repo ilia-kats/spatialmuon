4695416f8e42219c8ffa5fb06ddab30cda963949
Tim Treis <tim.treis@outlook.de>
2021-12-22 13:04:37 +0100

    Update test_spatialmodality.py

--- tests/_core/test_spatialmodality.py
+++ tests/_core/test_spatialmodality.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import spatialmuon
 from spatialmuon import SpatialModality, Raster
 import tifffile
 from xml.etree import ElementTree

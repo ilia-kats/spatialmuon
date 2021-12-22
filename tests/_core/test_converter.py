eed7c7823cee8ff7796f36964e020bf2464aeaa7
Tim Treis <tim.treis@outlook.de>
2021-12-22 15:10:13 +0100

    Update test_converter.py

--- tests/_core/test_converter.py
+++ tests/_core/test_converter.py
@@ -1,6 +1,6 @@
 import unittest
 import tifffile
-from spatialmuon import Converter
+import spatialmuon
 from pathlib import Path
 
 # Get current file and pre-generate paths and names
@@ -10,7 +10,7 @@ fpath = this_dir / "../data/ome_example.tiff"
 
 class Converter_TestClass(unittest.TestCase):
     def test_can_create_Raster_from_tiff(self):
-        c = Converter()
+        c = spatialmuon.Converter()
         ome_raster = c.raster_from_tiff(fpath)
         self.assertTrue(isinstance(ome_raster, spatialmuon.datatypes.raster.Raster))
 

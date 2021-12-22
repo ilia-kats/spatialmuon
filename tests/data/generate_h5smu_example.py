66b3749217b4479bb229df0d9b659754e83d4d56
Tim Treis <tim.treis@outlook.de>
2021-12-22 21:46:29 +0100

    Added generation of h5smu file

---
+++ tests/data/generate_h5smu_example.py
@@ -0,0 +1,24 @@
+from spatialmuon import Converter, SpatialMuData, SpatialModality
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+fpath_h5smu_example = this_dir / "h5smu_example.h5smu"
+fpath_ome_example = this_dir / "ome_example.tiff"
+fpath_ome_mask_left_eye = this_dir / "mask_left_eye.tiff"
+fpath_ome_mask_right_eye = this_dir / "mask_right_eye.tiff"
+fpath_ome_mask_mouth = this_dir / "mask_mouth.tiff"
+
+c = Converter()    
+    
+smudata = SpatialMuData(fpath_h5smu_example, backingmode="r+")
+
+mod = SpatialModality(coordinate_unit="μm")
+mod["ome"] = c.raster_from_tiff(fpath_ome_example)
+mod["left_eye"] = c.rastermask_from_tiff(fpath_ome_mask_left_eye)
+mod["right_eye"] = c.rastermask_from_tiff(fpath_ome_mask_right_eye)
+mod["mouth"] = c.rastermask_from_tiff(fpath_ome_mask_mouth)
+
+smudata["IMC"] = mod

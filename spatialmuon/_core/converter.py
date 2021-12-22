343453db1167e885d9364c20201ea7c543e1de67
Tim Treis <tim.treis@outlook.de>
2021-12-22 14:39:57 +0100

    mvp for converter

---
+++ spatialmuon/_core/converter.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Optional, Union, List
+import numpy as np
+import pandas as pd
+import tifffile
+import spatialmuon
+from xml.etree import ElementTree
+
+
+class Converter():
+        
+    def raster_from_tiff(self, path) -> spatialmuon.datatypes.raster.Raster:
+        """ Opens a .tiff and converts it to a spm.Raster(). """
+
+        ome = tifffile.TiffFile(path)
+        metadata = ElementTree.fromstring(ome.ome_metadata)[0]
+        for chld in metadata:
+            if chld.tag.endswith("Pixels"):
+                metadata = chld
+                break
+        channel_names = []
+        for channel in metadata:
+            if channel.tag.endswith("Channel"):
+                channel_names.append(channel.attrib["Fluor"])
+        var = pd.DataFrame({"channel_name": channel_names})
+        res = spatialmuon.Raster(
+            X=np.moveaxis(ome.asarray(), 0, -1), 
+            var=var
+        )
+
+        return(res)
+    
+    def rastermask_from_tiff(self, path) -> spatialmuon._core.masks.RasterMasks:
+        """ Opens a .tiff and converts it to a spm.RasterMask(). """
+
+        masks = np.asarray(tifffile.imread(path))
+        res = spatialmuon.RasterMasks(mask=masks)
+
+        return(res)
---
+++ tests/_core/test_converter.py
@@ -0,0 +1,19 @@
+import unittest
+import tifffile
+import spatialmuon
+
+# Get current file and pre-generate paths and names
+this_dir = Path(__file__).parent
+fpath = this_dir / "../data/ome_example.tiff"
+
+
+class Converter_conversions(unittest.TestCase):
+
+    def test_can_create_Raster_from_tiff(self):
+        c = spatialmuon.Converter()
+        ome_raster = c.raster_from_tiff(fpath)
+        self.assertTrue(isinstance(ome_raster, spatialmuon.datatypes.raster.Raster))
+
+
+if __name__ == "__main__":
+    unittest.main()
